45d9ce4f7980b47770f84604f75df7b9c5d7e786
Enzo Damasceno Falcão <enzodamasceno94@gmail.com>
2026-07-02 17:10:19 -0300

    chore: add ESLint and Prettier tooling

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  prettierConfig,
+);
